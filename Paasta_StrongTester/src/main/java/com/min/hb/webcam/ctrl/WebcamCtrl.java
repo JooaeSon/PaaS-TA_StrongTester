c850ca5c9a5fb8766b6e6a5a4e6bb39219db2152
JooaeSon <9apples1718@gmail.com>
2020-12-04 11:41:50 +0900

    2020. 12.04 작성자:손주애 내용:웹캠 수정

--- Paasta_StrongTester/src/main/java/com/min/hb/webcam/ctrl/WebcamCtrl.java
+++ Paasta_StrongTester/src/main/java/com/min/hb/webcam/ctrl/WebcamCtrl.java
@@ -2,10 +2,15 @@ package com.min.hb.webcam.ctrl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.min.hb.student.model.IService_Student;
+
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,7 +19,10 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class WebcamCtrl {
    private Logger log = LoggerFactory.getLogger(this.getClass());
-
+   
+   @Autowired
+	private IService_Student service;
+	
    @RequestMapping(value="/webcamTest.do", method=RequestMethod.POST)
    public String ChkWebcam(Model model, HttpServletRequest request){
       log.info("WebcamCtrl 웹캠 테스트 화면으로 이동: \t > {}");
@@ -34,9 +42,27 @@ public class WebcamCtrl {
     * 웹캠 확인이 완료되면 다시 시험응시 로그인화면으로 이동
     */
    @RequestMapping(value="/gobackHome.do", method=RequestMethod.POST)
-   public String FinishWebcamTest() {
+   public String FinishWebcamTest(Model model) {
       log.info("WebcamCtrl 웹캠 테스트 완료: \t > {}");
-      System.out.println("끝");
+      log.info("TestCtrl 수험자들의 응시 로그인 화면: \t > {}");
+		//시험 시간 조회
+		Map<String, Object> timeMap=service.selectTestTime();
+		if(timeMap!=null) {
+			System.out.println("timeMap"+timeMap);
+			String test_start=(String) timeMap.get("TEST_START");
+			String test_end=(String) timeMap.get("TEST_END");
+			log.info("test_start 시작시간: \t >{}", test_start);
+			log.info("test_end 종료시간: \t >{}", test_end);
+			
+			String date=test_start.substring(0, test_start.indexOf("T"));
+			String Stime=test_start.substring(test_start.indexOf("T")+1);
+			String Etime=test_end.substring(test_end.indexOf("T")+1);
+			log.info(date+"T"+Stime+"~"+Etime);
+			
+			model.addAttribute("date", date);
+			model.addAttribute("Stime", Stime);
+			model.addAttribute("Etime", Etime);
+		}
         
       return "tester/testInit";
    }
